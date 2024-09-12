f1ffc303c7e1fb3dddd159940c06be075b396512
Yohan Park <yohan9569@gmail.com>
2024-09-12 17:51:44 +0900

    refactor: update layout structure to align with router

---
+++ src/components/member/footer/MemberFooter.vue
@@ -0,0 +1,24 @@
+<!-- 
+ 담당자: 박요한 
+ 시작 일자: 2024.09.12
+ 설명 : MemberMainLayout의 아래 부분
+ ---------------------
+ 2024.09.12 박요한 | 컴포넌트 생성
+ -->
+
+<template>
+  <div class="footer">Footer입니다.</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.footer {
+  background-color: #f1f1f1; /* 푸터 배경색 */
+  padding: 20px; /* 내부 여백 */
+  text-align: center; /* 텍스트 중앙 정렬 */
+  color: #333; /* 텍스트 색상 */
+}
+</style>
