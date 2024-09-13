9a238ffeb4bcb5fee060d10bd623ece917172053
Yohan Park <yohan9569@gmail.com>
2024-09-13 16:31:30 +0900

    feat: mk home layout and its components

--- src/components/member/home-page/HighRatedAcademies.vue
+++ src/components/member/home-page/HighRatedAcademies.vue
@@ -1,21 +1,21 @@
 <!-- 
  담당자: 박요한
  시작 일자: 2024.09.13
- 설명 : 좋아요 많이 받은 리뷰 목록
+ 설명 : 평점이 높은 학원 목록
  ---------------------
  2024.09.13 박요한 | 컴포넌트 생성.
  -->
 
 <template>
   <div>
-    <h2>인기 리뷰</h2>
-    <!-- 리뷰 리스트 표시 -->
+    <h2>평점 높은 학원</h2>
+    <!-- 학원 리스트 표시 -->
   </div>
 </template>
 
 <script>
 export default {
-  name: "PopularReviews",
+  name: "HighRatedAcademies",
 };
 </script>
 
